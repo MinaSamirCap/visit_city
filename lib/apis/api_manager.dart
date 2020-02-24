486c72827b9c8ca7788103ac7b7b18134ce02a06
Mina Samir <MenaSamerCap@gmail.com>
2020-02-24 11:23:31 +0200

    removed not needed imports

--- lib/apis/api_manager.dart
+++ lib/apis/api_manager.dart
@@ -1,8 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http; // to avoid crashing with names ..
-import 'package:visit_city/models/category/category_response.dart';
-import 'package:visit_city/models/category/category_wrapper.dart';
+import '../models/category/category_wrapper.dart';
 import '../models/feedback/feedback_wrapper.dart';
 import '../models/message_model.dart';
 import '../models/feedback/feedback_send_model.dart';
